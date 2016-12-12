831809e5f5e50c8761b8b48a63cb0ac864ac321d
Mohit Chauhan <chauhanmohit86@live.com>
2016-12-12 18:32:04 +0530

    Added view to render data from API
    Added view to render data from API.
    Used async await approach for web api call for getEmployeeById().
    Also used Ajax Get with Partial View Approach to render certain data on
    screen.

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -8,14 +8,16 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using System.Net.Http.Formatting;
 
 namespace HelloWebAPIClient1.Controllers
 {
     public class EmployeeController : Controller
     {
-        // GET: Employee
+        // GET: Employee 
+       
         public ActionResult Index()
-        {
+        {            
             return View("EmployeeDashboard");
         }
 
@@ -23,23 +25,40 @@ namespace HelloWebAPIClient1.Controllers
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://localhost/HelloWebAPIApp/");
-
+            Employee empl1 = new Employee();
             // Add an Accept header for JSON format.
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync("api/Employee/");
 
-            HttpResponseMessage e = await client.GetAsync("api/Employee/");
-            if(e.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
-               var responsedata =  e.Content.ReadAsStringAsync().Result;
-                var JsonObject = Json(responsedata, JsonRequestBehavior.AllowGet).Data;
-                //foreach (var item in JsonObject)
-                //{
-
-                //}
-                return View(JsonObject);
+                string a = response.Content.ReadAsStringAsync().Result.Trim().Replace(@"\", "");
+                a = a.Substring(1, a.Length - 2);
+                JavaScriptSerializer j = new JavaScriptSerializer();
+                empl1 = j.Deserialize<Employee>(a);
+                //var empl1 = response.Content.ReadAsAsync<Employee>();
             }
             return View();
+        }
+
+        public async Task<ActionResult> GetEmployeeById(string empId)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/HelloWebAPIApp/");
+            Employee empl1 = new Employee();
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync("api/Employee/" + empId);
+
+            if (response.IsSuccessStatusCode)
+            {
+                //string a = response.Content.ReadAsStringAsync().Result.Trim().Replace(@"\", "");
+                //a = a.Substring(1, a.Length - 2);
+                //JavaScriptSerializer j = new JavaScriptSerializer();
+                //empl1 = j.Deserialize<Employee>(a);
+                empl1 = await response.Content.ReadAsAsync<Employee>();
+            }
+            return PartialView("_EmployeeById", empl1);
         }
     }
 }
